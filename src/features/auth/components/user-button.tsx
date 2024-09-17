33a16760b839475b93593928e25bfc1967c9011d
maxiaoteng <jizhuwo00@hotmail.com>
2024-09-17 18:44:22 +0800

    UserButton

---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { AvatarFallback } from "@radix-ui/react-avatar";
+import { Loader, LogOut } from "lucide-react";
+import { useCurrentUser } from "../hooks/use-current-user";
+
+export const UserButton = () => {
+  const { signOut } = useAuthActions();
+  const { data, isLoading } = useCurrentUser();
+  if (isLoading) {
+    return <Loader className="size-10 animate-spin text-muted-foreground" />;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const { image, name } = data;
+
+  const avatarFallback = name!.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback className="bg-sky-500 text-white w-full justify-center flex items-center">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem
+          onClick={() => {
+            signOut();
+          }}
+          className="h-10"
+        >
+          <LogOut className="size-4 mr-2" />
+          Log Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
