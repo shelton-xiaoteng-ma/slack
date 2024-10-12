5a199388cd6193944f8642ddb834300da274fae9
maxiaoteng <jizhuwo00@hotmail.com>
2024-10-12 22:41:18 +0800

    Editor component 1

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -3,6 +3,7 @@
 import { useGetChannel } from "@/features/channels/api/use-get-channel";
 import { useChannelId } from "@/hooks/use-channel-id";
 import { Loader, TriangleAlert } from "lucide-react";
+import { ChatInput } from "./chat-input";
 import { Header } from "./header";
 
 const ChannelIdPage = () => {
@@ -29,7 +30,8 @@ const ChannelIdPage = () => {
   return (
     <div className="h-full flex flex-col">
       <Header title={channel.name} />
-      Channel Id Page
+      <div className="flex-1"></div>
+      <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   );
 };
